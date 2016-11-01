0cde61e9819a9863daa65bc9e36d1ba7e5f618ab
Federico Espinosa Gómez <fesgo97@gmail.com>
2016-11-01 16:37:39 -0500

    Windows Manager

--- WM.vhd
+++ WM.vhd
@@ -7,25 +7,25 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 entity Win_man is
     Port ( rs1 : in  STD_LOGIC_VECTOR (4 downto 0);
            rs2 : in  STD_LOGIC_VECTOR (4 downto 0);
-           rsd : in  STD_LOGIC_VECTOR (4 downto 0);
+           rd : in  STD_LOGIC_VECTOR (4 downto 0);
            op : in  STD_LOGIC_VECTOR (1 downto 0);
            op3 : in  STD_LOGIC_VECTOR (5 downto 0);
            cwp : in  STD_LOGIC;
            ncwp : out  STD_LOGIC;
-           srs1 : out  STD_LOGIC_VECTOR (5 downto 0);
-           srs2 : out  STD_LOGIC_VECTOR (5 downto 0);
-           srd : out  STD_LOGIC_VECTOR (5 downto 0));
+           nrs1 : out  STD_LOGIC_VECTOR (5 downto 0);
+           nrs2 : out  STD_LOGIC_VECTOR (5 downto 0);
+           nrd : out  STD_LOGIC_VECTOR (5 downto 0));
 end Win_man;
 
 architecture Behavioral of Win_man is
 
 signal rs1n: integer range 0 to 39:=0;
 signal rs2n: integer range 0 to 39:=0;
-signal rsdn: integer range 0 to 39:=0;
+signal rdn: integer range 0 to 39:=0;
 
 
 begin
-process(cwp,rs1,rs2,rsd,op,op3)
+process(cwp,rs1,rs2,rd,op,op3)
 begin
 
 ------Save-and-Restore------
@@ -76,19 +76,19 @@ begin
 	end if;
 
 
-	----Evaluacion tipo de registro destino(rsd)----
+	----Evaluacion tipo de registro destino(rd)----
 
-	if(rsd>="00000" and rsd<="00111")then --Evaluacion de los registros globales
-		rsdn <= conv_integer(rsd);
+	if(rd>="00000" and rd<="00111")then --Evaluacion de los registros globales
+		rdn <= conv_integer(rd);
 	else
-		if(rsd >= "11000" and rsd <= "11111")then -- Evaluacion de los registros de entrada
-			rsdn <= conv_integer(rsd) - (conv_integer(cwp)*16);
+		if(rd >= "11000" and rd <= "11111")then -- Evaluacion de los registros de entrada
+			rdn <= conv_integer(rd) - (conv_integer(cwp)*16);
 		else
-			if(rsd >= "10000" and rsd <= "10111")then -- Evaluacion de los registros locales
-				rsdn <= conv_integer(rsd)+(conv_integer(cwp)*16);
+			if(rd >= "10000" and rd <= "10111")then -- Evaluacion de los registros locales
+				rdn <= conv_integer(rd)+(conv_integer(cwp)*16);
 			else
-				if(rsd >= "01000" and rsd <= "01111")then -- Evaluacion de los registros de salida
-					rsdn <= conv_integer(rsd) + (conv_integer(cwp)*16);
+				if(rd >= "01000" and rd <= "01111")then -- Evaluacion de los registros de salida
+					rdn <= conv_integer(rd) + (conv_integer(cwp)*16);
 				end if;
 			end if;
 		end if;
@@ -97,8 +97,8 @@ begin
 -----------------------------------------------------------------
 end process;
 
-srs1 <= conv_std_logic_vector(rs1n,6);
-srs2 <= conv_std_logic_vector(rs2n,6);
-srd <= conv_std_logic_vector(rsdn,6);
+nrs1 <= conv_std_logic_vector(rs1n,6);
+nrs2 <= conv_std_logic_vector(rs2n,6);
+nrd <= conv_std_logic_vector(rdn,6);
 
 end Behavioral;
